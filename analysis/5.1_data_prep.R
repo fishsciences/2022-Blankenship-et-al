429c9cddb9ac78c53c127e9b84c36b96390ad8c8
Myfanwy <myfanwy@github.com>
2021-09-28 15:16:36 -0700

    move more data cleaning code from 5.1_analysis.R

--- analysis/5.1_data_prep.R
+++ analysis/5.1_data_prep.R
@@ -33,6 +33,26 @@ names(hydros) = gsub("livecar_simulationv3run[0-9](.*)\\.csv$", "\\1", basename(
 hydros = as.data.frame(hydros)
 tidal5.1 = cbind(tidal01, hydros)
 
+# munge data
+#-------------------------------------------------------#
+tidal5.1$abs_dist_m = abs(tidal5.1$Distance_m)
+mu = mean(tidal5.1$abs_dist_m)
+sd = sd(tidal5.1$abs_dist_m)
+tidal5.1$stand_dist = scale(tidal5.1$abs_dist_m, mu, sd) # scale distances
+
+# stdcurvs - from elaphos::StdCrvKey.
+stdcrvs = structure(list(
+  StdCrvID = c("an-2019-10-29", "ds-2019-10-29"),
+StdCrvAlpha_lnForm = c(18.441, 20.583),
+StdCrvBeta_lnForm = c(-1.545, -1.486)),
+row.names = c(3L, 4L), class = "data.frame")
+
+stdcrvs$Sp_abbv = c("Anchovy", "Delta Smelt")
+i = match(tidal5.1$Species, stdcrvs$Sp_abbv)
+
+tidal5.1$std_alpha = stdcrvs$StdCrvAlpha_lnForm[i]
+tidal5.1$std_beta = stdcrvs$StdCrvBeta_lnForm[i]
+
 if(!dir.exists(res_dir)) dir.create(res_dir)
 
 saveRDS(tidal5.1, file.path(res_dir, "tidal5.1_w_hydro.rds"))
